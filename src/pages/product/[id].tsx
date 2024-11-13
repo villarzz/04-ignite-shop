bc1ab98c052db0cb5597ee0fb18d2aee0283ef68
hugo vilar <hugovilar04@gmail.com>
2024-11-13 14:43:27 -0300

    Refactor product page layout

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,5 +1,28 @@
-import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 
 export default function Product() {
-  return <h1>Product!</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
+          est vel minima, deserunt hic nobis nemo? In tempora quaerat hic id
+          impedit sunt placeat, delectus doloribus temporibus ab, atque veniam!
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
