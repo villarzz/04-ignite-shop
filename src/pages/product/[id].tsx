aacea38a00ccba458ef692caeb3d8bf463c596f3
hugo vilar <hugovilar04@gmail.com>
2024-11-14 08:28:58 -0300

    Update product page and home page interfaces

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,28 +1,72 @@
+import { stripe } from "@/lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "@/styles/pages/product";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import Stripe from "stripe";
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+    description: string;
+  };
+}
+
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} alt="" width={520} height={480} />
+      </ImageContainer>
 
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis
-          est vel minima, deserunt hic nobis nemo? In tempora quaerat hic id
-          impedit sunt placeat, delectus doloribus temporibus ab, atque veniam!
-        </p>
+        <p>{product.description}</p>
 
-        <button>
-          Comprar agora
-        </button>
+        <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "prod_RCo3cLlkDmhAcO" } }],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params?.id;
+  const product = await stripe.products.retrieve(productId as string, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hora
+  };
+};
