993ab1f4be9207b96cbddf00f845d9e85cea3f95
hugo vilar <hugovilar04@gmail.com>
2024-11-21 11:46:18 -0300

    Update success page to display customer name and product image

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,17 +1,62 @@
-import { ImageContainer, SuccessContainter } from "@/styles/pages/success";
+import Stripe from "stripe";
 import Link from "next/link";
+import Image from "next/image";
+import { stripe } from "@/lib/stripe";
+import { GetServerSideProps } from "next";
+import { ImageContainer, SuccessContainter } from "@/styles/pages/success";
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainter>
       <h1>Compra efetuada!</h1>
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={120} height={110} alt=""></Image>
+      </ImageContainer>
       <p>
-        Uhuul <strong>Hugo Vilar</strong>, sua <strong>camiseta</strong> já está
+        Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está
         a caminho da sua casa!
       </p>
 
       <Link href="/">Voltar ao catálogo</Link>
     </SuccessContainter>
   );
-}
+} 
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if(!query.session_id) {
+    return {
+      redirect:{
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+  
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details?.name;
+
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
